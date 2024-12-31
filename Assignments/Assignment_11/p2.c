a089833e6466b92f6e206b09aa81384324296e89
Shriraj Deshpande <srdeshpande1122@gmail.com>
2024-12-31 19:10:02 +0530

    Create p2.c

---
+++ Assignments/Assignment_11/p2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+        int size;
+        printf("Enter the size : \n");
+        scanf("%d",&size);
+        int arr1[size],arr2[size];
+        for(int i=0;i<size;i++){
+
+                printf("Enter the element %d : ",i+1);
+                scanf("%d",&arr1[i]);
+        }
+        for(int i=0;i<size;i++){
+
+                arr2[i]=arr1[i];
+        }
+        for(int i=size-1;i>=0;i--){
+                printf("%d\n",arr2[i]);
+
+        }
+}
