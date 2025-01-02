1a5516299aff37af1e8dfcab9bf1e963c8485844
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-02 16:43:09 +0530

    Create p8.c

---
+++ Assignments/Assignment-14/p8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+
+        int row,num=1;
+        printf("Enter the no of rows : \n");
+        scanf("%d",&row);
+        for(int i=1;i<=row;i++){
+
+                for(int j=1;j<=row;j++){
+
+                        if((i+j)%2==0){
+
+                                printf("%d\t",num);
+                                num++;
+                        }else{
+
+                                printf("%d\t",(num*num));
+                                num++;
+                        }
+                }printf("\n\n");
+        }
+}
