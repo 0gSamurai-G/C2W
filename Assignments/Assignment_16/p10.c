16c9592f169d838f03910e899ddd789a8f535213
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-03 19:28:27 +0530

    Create p10.c

---
+++ Assignments/Assignment_16/p10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+
+        int row;
+        printf("Enter the no of rows : \n");
+        scanf("%d",&row);
+        int size=(row*2)-1;
+        for(int i=0;i<row;i++){
+
+                for(int j=1;j<=size;j++){
+
+                        if((j<(i+1))||(j>(size-i))){
+
+                                printf("\t");
+                        }else{
+
+                                printf("%c\t",(65+i));
+                        }
+                }printf("\n\n");
+        }
+}
