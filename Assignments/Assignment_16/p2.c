48cd8d23c012062a53ae4ebf998b971b3699d6a9
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-03 16:59:54 +0530

    Create p2.c

---
+++ Assignments/Assignment_16/p2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+
+        int row;
+        printf("Enter the no of rows : \n");
+        scanf("%d",&row);
+        int size=(row*2)-1,num=1;
+        for(int i=0;i<row;i++){
+
+                for(int j=1;j<=size;j++){
+
+                        if((j<row-i)||(j>row+i)){
+
+                                printf("\t");
+                        }else{
+
+                                printf("%d\t",num);
+                                num++;
+                        }
+                }printf("\n\n");
+        }
+}
