8ad3a5d230bd0ab5f02d6b448147babbe27bab7c
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-03 19:28:52 +0530

    Create p9.c

---
+++ Assignments/Assignment_16/p9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+
+        int row;
+        printf("Enter the no of rows : \n");
+        scanf("%d",&row);
+        int size=(row*2)-1;
+        for(int i=0;i<row;i++){
+
+                int num=1;
+                for(int j=1;j<=size;j++){
+
+                        if((j<(i+1))||(j>(size-i))){
+
+                                printf("\t");
+                        }else{
+
+                                printf("%d\t",num);
+                                num++;
+                        }
+                }printf("\n\n");
+        }
+}
