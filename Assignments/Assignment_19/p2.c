07257b6be29885d6deb23780981918db3b473aec
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-05 20:25:38 +0530

    Create p2.c

---
+++ Assignments/Assignment_19/p2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+
+        int size;
+        printf("Enter the size of array:\n");
+        scanf("%d",&size);
+        int arr[size];
+        for(int i=0;i<size;i++){
+
+                printf("Enter the arr element %d: ",i+1);
+                scanf("%d",&arr[i]);
+
+        }printf("Composite nos are :\n");
+        for(int i=0;i<size;i++){
+
+                int count=0;
+                for(int j=2;j<(arr[i]);j++){
+
+                        if((arr[i])%j==0) count++;
+                }if(count>0) printf("%d\n",i);
+        }
+}
