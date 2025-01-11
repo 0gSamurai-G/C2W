2cc85f4c730e4a6856810ac530d0355924d6c4a2
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-11 18:56:26 +0530

    Update p5.c

--- Assignments/Assignment_25/p5.c
+++ Assignments/Assignment_25/p5.c
@@ -1,28 +1,37 @@
-#include<stdio.h>                                              void main(){
+#include<stdio.h>
+void main(){
 
         int row,col,sum=0,max,flag=1,index=0;
-        printf("Enter the no. of rows and columns of 2D array:\
-n");
+        printf("Enter the no. of rows and columns of 2D array:\n");
         scanf("%d%d",&row,&col);
         int arr[row][col],arr1[col];
         printf("Enter the 2D array elements:\n");
         for(int i=0;i<row;i++){
-                                                                               sum=0;                                                         for(int j=0;j<col;j++){                                                                                                               scanf("%d",&arr[j][i]);                                        sum+=arr[j][i];
+
+                sum=0;
+                for(int j=0;j<col;j++){
+
+                        scanf("%d",&arr[j][i]);
+                        sum+=arr[j][i];
                 }arr1[i]=sum;
         }max=arr1[0];
-        for(int i=1;i<col;i++){                                
+        for(int i=1;i<col;i++){
+
                 if(arr1[0]==arr1[i]){
 
                         flag=0;
-                }else{flag=1;}                                                 if(arr1[i]>max){
+                }else{flag=1;}
+                if(arr1[i]>max){
 
                         max=arr1[i];
                         index=i;
                 }
         }if(flag==0){
 
-                printf("Sum of all columns is same!\n");               }else{
+                printf("Sum of all columns is same!\n");
+        }else{
 
-                printf("Max sum is found at column %d:%d\n",index+1,max);                                                             }
+                printf("Max sum is found at column %d:%d\n",index+1,max);
+        }
 
 }
