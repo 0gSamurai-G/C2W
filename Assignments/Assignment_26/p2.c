691d97673d2bb913ea0aff11c08be32854e93211
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-01-15 17:29:58 +0530

    Update Assignment_26

---
+++ Assignments/Assignment_26/p2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+
+        int size,even=0,odd=0;
+        printf("Enter the size of array:\n");
+        scanf("%d",&size);
+        int arr[size];
+        printf("Enter the array elements:\n");
+        for(int i=0;i<size;i++){
+
+                scanf("%d",&arr[i]);
+                if((arr[i])%2==0){
+
+                        even+=(arr[i]*arr[i]);
+                }else{
+
+                        odd+=(arr[i]*arr[i]);
+                }
+        }printf("Difference of sum of square of even and odd elements:%d\n",even-odd);
+}
