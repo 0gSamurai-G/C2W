7b801facd77d038782b185594dd5b6e2979d5255
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-02-20 10:00:55 +0530

    Update JAVA

---
+++ JAVA/6.For_loop_1/p1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Nat{
+
+        public static void main(String[] args){
+
+                Scanner scan=new Scanner(System.in);
+                System.out.println("Enter a number:");
+                int x=scan.nextInt();
+                for(int i=1;i<=x;i++){
+
+                        System.out.println(i);
+                }
+        }
+}
