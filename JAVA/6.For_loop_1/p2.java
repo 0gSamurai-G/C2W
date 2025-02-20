383201f0eb409ee3abeeb5dabb906ede4f8078ef
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-02-20 10:02:35 +0530

    Update 6.For_loop_1

---
+++ JAVA/6.For_loop_1/p2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Who{
+
+        public static void main(String[] args){
+
+                Scanner scan=new Scanner(System.in);
+                System.out.println("Enter a number:");
+                int x=scan.nextInt();
+                for(int i=0;i<=x;i++){
+
+                        System.out.println(i);
+                }
+        }
+}
