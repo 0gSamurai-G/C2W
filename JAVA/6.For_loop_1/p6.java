773c8ed780cabcc470b69bc0c8976b9ab2be81f8
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-02-20 10:13:25 +0530

    Update 6.For_loop_1

---
+++ JAVA/6.For_loop_1/p6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Num10{
+
+        public static void main(String[] args){
+
+                Scanner scan=new Scanner(System.in);
+                System.out.println("Enter a number:");
+                int x=scan.nextInt();
+                for(int i=x;i>=10;i--){
+
+                        System.out.println(i);
+
+                }
+        }
+}
