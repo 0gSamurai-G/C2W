ac99111c215d2f136eedd9a859b8fa075e9d73b3
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-02-20 07:56:07 +0530

    Update IF ELSE-2

---
+++ JAVA/IF ELSE-2/p6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Age{
+
+        public static void main(String[] args){
+
+                Scanner scan=new Scanner(System.in);
+                System.out.println("Enter your age:");
+                int age=scan.nextInt();
+                if(age>=18){
+
+                        System.out.println("Eligible to vote");
+                }else{
+
+                        System.out.println("Not eligible to vote");
+                }
+        }
+}
