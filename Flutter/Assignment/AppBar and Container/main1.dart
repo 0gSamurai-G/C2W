173914215cd2f94a15fbed1756cfad35507ac9f6
Shriraj Deshpande <srdeshpande1122@gmail.com>
2025-06-23 14:04:21 +0530

    Update and rename main.dart to main1.dart

---
+++ Flutter/Assignment/AppBar and Container/main1.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.folder))
+          ],
+          title: Text("AppBar"),
+          backgroundColor: Colors.deepPurpleAccent,
+          centerTitle: true,
+        ),
+      )
+    );
+  }
+}
